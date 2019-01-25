96c9da2d4581aaa3883b26d2ee98b426910e6316
Arram Mandel <airum82@gmail.com>
2019-01-24 18:24:58 -0700

    Initial commit after setup

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => {
+  return (
+    <header>
+      I'm a header
+    </header>
+  )
+}
+
+export default Header;
