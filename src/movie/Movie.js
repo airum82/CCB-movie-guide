b47a7084d42869efb3eb3928c66ed3bad59e3430
Arram Mandel <airum82@gmail.com>
2019-01-25 08:40:39 -0700

    Add setup for movie and movie container

---
+++ src/movie/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Movie = props => {
+  return (
+    <article>
+      I'm a movie
+    </article>
+  )
+}
+
+export default Movie;
---
+++ src/movies-container/MoviesContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieContainer = props => {
+  return (
+    <section>
+      Here are movies
+    </section>
+  )
+}
+
+export default MovieContainer;
