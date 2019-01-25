edc5bc153fc6da706048bd8d5a4a6b289270dd34
Arram Mandel <airum82@gmail.com>
2019-01-25 11:42:27 -0700

    Create api movie details method

--- src/APImethods.js
+++ src/APImethods.js
@@ -3,5 +3,11 @@ import { APIkey } from './APIkey';
 export const getMoviesByCategory = category => {
   return fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${APIkey}&language=en-US`)
     .then(response => response.json())
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
+}
+
+export const getMovieDetails = movieId => {
+  return fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIkey}&language=en-US`)
+    .then(response => response.json())
+    .catch(err => console.log(err));
 }
